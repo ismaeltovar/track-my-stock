1a32e7c640c6ae63b8431f798f9d02077962302b
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-10-07 22:19:07 -0500

    Turned UsrStockList into UsrStockListItem

--- src/components/UsrStockListItem/UsrStockListItem.js
+++ src/components/UsrStockListItem/UsrStockListItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { stocks, usrStocks } from '../../stocks.js';
-import './UsrStockList.css';
+import './UsrStockListItem.css';
 
 /* possibly use to get logo of stocks
 function getLogo(stockName) {
@@ -8,24 +7,12 @@ function getLogo(stockName) {
 }
 */
 
-function UsrStockList() {
-
-  //sorts stocks in descending order based on price
-  usrStocks.sort((a, b) => {
-    return b.prices.Oct - a.prices.Oct;  //uses the stock's october price
-  });
-  
+export default function UsrStockListItem(props) {
   return (
-    <ul className="usr-stock-list">
-      {usrStocks.map(({name, symbol, logo, prices}) => (
-        <div className={`usr-stock-li ${name}`}> 
-            <p className="usr-stock-logo">{logo}</p> {/* Custom company logos to stock items for better UI */}
-            <p className="usr-stock-name-symbol">{name} ({symbol})</p>
-            <p className="usr-stock-price">${prices.Oct} USD</p>
-        </div>
-      ))}
-    </ul>
+    <button className="usr-stock-li" id={props.symbol} onClick={props.onClick}>
+      <p className="usr-stock-logo">{props.logo}</p> {/* Custom company logos to stock items for better UI */}
+      <p className="usr-stock-name-symbol">{props.name} ({props.symbol})</p>
+      <p className="usr-stock-price">${props.prices.Oct} USD</p>
+    </button>
   );
 }
-
-export default UsrStockList;
