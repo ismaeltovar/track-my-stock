cee300bcd184daeabc80b883815ef6a76bef4c84
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-10-07 21:53:05 -0500

    Added drawer button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ export default function Header(props) {
 
   return (
     <header>
+      <button id="drawer-button" onClick={props.showDrawerClick}><i class="bi bi-list"></i></button>
       <h1 id="pg-title">{props.pgTitle}</h1>
     </header>
   );
