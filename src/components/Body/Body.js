65bc25cf88ebcd22d9ac4218ac5b9b0fc1600d9a
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-10-08 20:13:37 -0500

    Changed styling for UsrStockListItems (changed selected list item's border color)

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -14,13 +14,12 @@ function MyStocks() {
   });
 
   const [selectedStock, setSelectedStock] = useState(findStock(usrStocks[0].symbol)); //finds first stock in user stock list
-
   
   return (
     <body> 
       <ul className="usr-stock-list">
       {usrStocks.map(({name, symbol, logo, prices}) => (
-        <UsrStockListItem name={name} symbol={symbol} logo={logo} prices={prices} onClick={() => setSelectedStock(findStock(symbol))}/> 
+        <UsrStockListItem selected={symbol === selectedStock.symbol ? true : false} name={name} symbol={symbol} logo={logo} prices={prices} onClick={() => setSelectedStock(findStock(symbol))}/> 
       ))}
       </ul>
       <UsrStockGraph selectedStockPrices={selectedStock.prices}/>
--- src/components/UsrStockListItem/UsrStockListItem.js
+++ src/components/UsrStockListItem/UsrStockListItem.js
@@ -9,7 +9,7 @@ function getLogo(stockName) {
 
 export default function UsrStockListItem(props) {
   return (
-    <button className="usr-stock-li" id={props.symbol} onClick={props.onClick}>
+    <button className={`usr-stock-li ${props.selected ? "selected-li" : "unselected-li"}`} id={props.symbol} onClick={props.onClick}>
       <p className="usr-stock-logo">{props.logo}</p> {/* Custom company logos to stock items for better UI */}
       <p className="usr-stock-name-symbol">{props.name} ({props.symbol})</p>
       <p className="usr-stock-price">${props.prices.Oct} USD</p>
