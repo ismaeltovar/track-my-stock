b4392fbbb9cc6b67b185462c15b4c5fd74e763ee
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-10-08 19:23:02 -0500

    Created graph for UsrStocks page

--- src/components/UsrStockGraph/UsrStockGraph.js
+++ src/components/UsrStockGraph/UsrStockGraph.js
@@ -1,17 +1,60 @@
-//Possibly use the ChartJS library (licensed under MIT license) for the graph
-
-import React from 'react';
-import Stocks from '../../stocks.js';
+//Using the ChartJS library (licensed under MIT license) for the graph
+import React, { useEffect, useState } from 'react';
+import Chart from 'chart.js/auto';  //imports & registers controllers, elements, scales, and plugins for use
 import './UsrStockGraph.css';
+import '../../index.css'
 
-function StockDataGraph() {
+function getColorVar(colorVar) {
+  return getComputedStyle(document.documentElement).getPropertyValue(colorVar);
+}
+
+var graphObj;
+export default function UsrStockGraph(props) {
+
+  useEffect(() => {
+
+    const labels = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+    
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Price',
+        backgroundColor: getColorVar('--black'),
+        borderColor: getColorVar('--green'),
+        data: props.selectedStockPrices,
+      }]
+    };
+    
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    if (graphObj === undefined || graphObj === null) {
+      graphObj = new Chart(document.getElementById('graph-canvas'), config);
+    } else {
+      graphObj.destroy();
+      graphObj = new Chart(document.getElementById('graph-canvas'), config);
+    }
+  });
+
   return (
-    <div className="usr-stock-graph">
-      <p>This is where the graph should go.</p>
-      <canvas id="graph"></canvas>
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <div id="usr-stock-graph-container">
+      <canvas id="graph-canvas">This is were the graph should go.</canvas>
     </div>
   );
 }
-
-export default StockDataGraph;
