796cd4813800a08bbcfa4523bebe8776448b62ed
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-10-08 19:23:30 -0500

    Added graph to Body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import {stocks, usrStocks} from "../../stocks";
 import UsrStockListItem from "../UsrStockListItem/UsrStockListItem";
+import UsrStockGraph from "../UsrStockGraph/UsrStockGraph";
+
+function findStock(symbol) {
+  return usrStocks.find(stock => stock.symbol === symbol);
+}
 
 function MyStocks() {
-  const [selectedStock, setSelectedStock] = useState("GOOGL");
-
   //sorts stocks in descending order based on price
   usrStocks.sort((a, b) => {
     return b.prices.Oct - a.prices.Oct;  //uses the stock's october price
   });
+
+  const [selectedStock, setSelectedStock] = useState(findStock(usrStocks[0].symbol)); //finds first stock in user stock list
+
   
   return (
     <body> 
       <ul className="usr-stock-list">
       {usrStocks.map(({name, symbol, logo, prices}) => (
-        <UsrStockListItem name={name} symbol={symbol} logo={logo} prices={prices} onClick={() => setSelectedStock(symbol)}/> 
+        <UsrStockListItem name={name} symbol={symbol} logo={logo} prices={prices} onClick={() => setSelectedStock(findStock(symbol))}/> 
       ))}
       </ul>
-      <UsrStockGraph selectedStock={selectedStock}/>
+      <UsrStockGraph selectedStockPrices={selectedStock.prices}/>
     </body>
   );
 }
