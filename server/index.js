5d12cd7564e79e992d665f9a2103a4efbd128e60
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-11-28 11:46:45 -0600

    Added method for making API calls though server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 var request = require('request');
 const path = require('path');
-//Express framework
 const express = require("express");
+const bp = require('body-parser');
 const app = express();
 const dotenv = require('dotenv');
 const result = dotenv.config()
@@ -16,15 +16,34 @@ let API_KEY = result.parsed['API_KEY'];
 
 const PORT = process.env.PORT || 3001;
 
+//Body-parser middleware
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended : true}));
+
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get("/api", (req, res) => {
-  res.json({message: "API request received."});
+  res.json({message: "Wrong method of request. Try using a POST request instead."});
 });
 
-app.get("/IBM", (req, res) => {
+//POST request for API calls
+app.post('/api', (req, res) => {
+  let symbol = req.body.symbol;
+  res.json(makeAPIRequest(symbol));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+let makeAPIRequest = (symbol) => {
   let stockData;
-  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${API_KEY}`;
+  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${API_KEY}`;
 
   request.get({
     url: url,
@@ -36,18 +55,9 @@ app.get("/IBM", (req, res) => {
     } else if (res.statusCode !== 200) {
       console.log('Status:', res.statusCode);
     } else {
-      // data is successfully parsed as a JSON object:
-      console.log(data);
+      console.log(`Get request successful for: ${symbol}`);
       stockData = data;
     }
   });
-  res.json(stockData);
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+  return stockData;
+}
